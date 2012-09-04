56afa55df110cf395e75511acf828e928ade6895
kromar <grauer.d@gmail.com>
2012-09-04 09:50:05 -0700

    renamed addon; added edge and face selection

--- addons_contrib/mesh_IndexMarker.py
+++ addons_contrib/mesh_IndexMarker.py
@@ -20,9 +20,12 @@
 
 """
 todo:
-    - testing
+    - enable vertex select mode when switching to edit mode
 
 changelog:
+    "version": (1, 1, 1),
+        - added face and edge selection
+        
     "version": (1,1,0)
         - added option to toggle indices in viewport; enables debug mode
     
@@ -35,45 +38,62 @@ changelog:
 import bpy
 
 bl_info = {
-    "name": "Vertex Selector",
+    "name": "Index Marker",
     "author": "Daniel Grauer (kromar)",
-    "version": (1, 1, 0),
+    "version": (1, 1, 1),
     "blender": (2, 6, 3),
     "category": "Mesh",
     "category": "kromar",
-    "location": "Properties space > Data > Vertex Selector",
-    "description": "select vertices by index numbers",
+    "location": "Properties space > Data > Index Marker",
+    "description": "select vertices, faces and edges by index numbers",
     "warning": "", # used for warning icon and text in addons panel
     "wiki_url": "",
     "tracker_url": ""}
 
 print(" ")
 print("*------------------------------------------------------------------------------*")
-print("*                          VertexSelector                      *")
+print("*                          IndexMarker                      *")
 print(" ")
 
 
-def VS_select(indexList):
+def IM_select(indexList,type):
     #indices = [4608]    #add more indices: [0, 1, 4, 722]
     mesh =  bpy.context.active_object.data    
     bpy.ops.object.mode_set(mode = 'EDIT', toggle = False)
     bpy.ops.mesh.reveal()
     bpy.ops.mesh.select_all(action='DESELECT')    
     bpy.ops.object.mode_set(mode = 'OBJECT', toggle = False)
-    
-    for vert in mesh.vertices:
-        #print(vert.index)
-        for target in indexList:
-            if vert.index == target:
-                vert.select = True
-                print(target, ' vertex selected')       
+   
+    if type == 'vertex':
+        for vert in mesh.vertices:
+            #print(vert.index)
+            for target in indexList:
+                if vert.index == target:
+                    vert.select = True
+                    print(target, ' vertex selected')   
+                    
+    if type == 'face':
+        for face in mesh.polygons:
+            #print(face.index)
+            for target in indexList:
+                if face.index == target:
+                    face.select = True
+                    print(target, ' face selected') 
+                    
+    if type == 'edge':
+        for edge in mesh.edges:
+            #print(edge.index)
+            for target in indexList:
+                if edge.index == target:
+                    edge.select = True
+                    print(target, ' edge selected') 
             
     
     bpy.ops.object.mode_set(mode = 'EDIT', toggle = False)
 
-def VS_show_extra_indices(self, context):
+def IM_show_extra_indices(self, context):
     mesh =  bpy.context.active_object.data 
-    config = bpy.data.scenes[0].CONFIG_VertexSelector
+    config = bpy.data.scenes[0].CONFIG_IndexMarker
     print("Show indices: ", config.show_extra_indices)
     
     if config.show_extra_indices == True:
@@ -91,13 +111,13 @@ def VS_show_extra_indices(self, context):
 #======================================================================#        
 class UIElements(bpy.types.PropertyGroup):
 
-    get_indices = bpy.props.StringProperty(name="vertex index:", description="input vertex indices here for selection. example: 1,2,3")
-    show_extra_indices = bpy.props.BoolProperty(name="Show selected indices", default=False, description="Display the index numbers of selected vertices, edges, and faces. Note: enables debug mode", update=VS_show_extra_indices)
+    get_indices = bpy.props.StringProperty(name="index:", description="input vertex, face or edge indices here for selection. example: 1,2,3")
+    show_extra_indices = bpy.props.BoolProperty(name="Show selected indices", default=False, description="Display the index numbers of selected vertices, edges, and faces. Note: enables debug mode", update=IM_show_extra_indices)
 
 
-class OBJECT_PT_VertexSelector(bpy.types.Panel):
-    bl_label = "VertexSelector"
-    bl_idname = "OBJECT_PT_VertexSelector"
+class OBJECT_PT_IndexMarker(bpy.types.Panel):
+    bl_label = "IndexMarker"
+    bl_idname = "OBJECT_PT_IndexMarker"
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
     bl_context = "data"
@@ -105,7 +125,7 @@ class OBJECT_PT_VertexSelector(bpy.types.Panel):
     
     def draw(self, context):
     
-        config = bpy.data.scenes[0].CONFIG_VertexSelector
+        config = bpy.data.scenes[0].CONFIG_IndexMarker
         layout = self.layout
         ob = context.object
         type = ob.type.capitalize()
@@ -121,17 +141,21 @@ class OBJECT_PT_VertexSelector(bpy.types.Panel):
             row = layout.column()            
             row.prop(config, "show_extra_indices")
             row.prop(config, "get_indices")
-            row.operator("mesh.vertexselector_select", text="select vertices")
+            
+            row = layout.row() 
+            row.operator("mesh.vertex_select", text="vertices")
+            row.operator("mesh.face_select", text="faces")
+            row.operator("mesh.edge_select", text="edges")
 
             
-class OBJECT_OP_Select(bpy.types.Operator):
-    bl_idname = "mesh.vertexselector_select"
-    bl_label = "Select"
+class OBJECT_OP_SelectVertices(bpy.types.Operator):
+    bl_idname = "mesh.vertex_select"
+    bl_label = "Select vertex"
     bl_description = "select vertices"
             
     def execute(self, context):
         #get arguments from UIElemtnts
-        config = bpy.data.scenes[0].CONFIG_VertexSelector
+        config = bpy.data.scenes[0].CONFIG_IndexMarker
         
         #lets convert the values to int and pass the list to the select function
         indexList=[]
@@ -140,9 +164,49 @@ class OBJECT_OP_Select(bpy.types.Operator):
             indexList.append(int(i))
             
         #print(indexList)
-        VS_select(indexList)
+        type='vertex'
+        IM_select(indexList,type)
         return {'FINISHED'} 
+ 
+class OBJECT_OP_SelectFaces(bpy.types.Operator):
+    bl_idname = "mesh.face_select"
+    bl_label = "Select face"
+    bl_description = "select faces"
+            
+    def execute(self, context):
+        #get arguments from UIElemtnts
+        config = bpy.data.scenes[0].CONFIG_IndexMarker
         
+        #lets convert the values to int and pass the list to the select function
+        indexList=[]
+        detectList = config.get_indices.split(',')
+        for i in detectList:
+            indexList.append(int(i))
+            
+        #print(indexList)
+        type='face'
+        IM_select(indexList,type)
+        return {'FINISHED'} 
+
+class OBJECT_OP_SelectEdges(bpy.types.Operator):
+    bl_idname = "mesh.edge_select"
+    bl_label = "Select edge"
+    bl_description = "select edges"
+            
+    def execute(self, context):
+        #get arguments from UIElemtnts
+        config = bpy.data.scenes[0].CONFIG_IndexMarker
+        
+        #lets convert the values to int and pass the list to the select function
+        indexList=[]
+        detectList = config.get_indices.split(',')
+        for i in detectList:
+            indexList.append(int(i))
+            
+        #print(indexList)
+        type='edge'
+        IM_select(indexList,type)
+        return {'FINISHED'}         
 
 
 #======================================================================# 
@@ -150,15 +214,15 @@ class OBJECT_OP_Select(bpy.types.Operator):
 #======================================================================#
 def register():
     bpy.utils.register_module(__name__)
-    bpy.types.Scene.CONFIG_VertexSelector = bpy.props.PointerProperty(type = UIElements)
+    bpy.types.Scene.CONFIG_IndexMarker = bpy.props.PointerProperty(type = UIElements)
 
     
 def unregister():
     bpy.utils.unregister_module(__name__)
-    if bpy.context.scene.get('CONFIG_VertexSelector') != None:
-        del bpy.context.scene['CONFIG_VertexSelector']
+    if bpy.context.scene.get('CONFIG_IndexMarker') != None:
+        del bpy.context.scene['CONFIG_IndexMarker']
     try:
-        del bpy.types.Scene.CONFIG_VertexSelector
+        del bpy.types.Scene.CONFIG_IndexMarker
     except:
         pass
 
